8a909e36da0e5ae2a4f8e6f751f32dd7cd367945
Dominik Bilski <dbilski@edu.cdv.pl>
2020-03-05 17:21:40 +0100

    First exercise

---
+++ python/1_podstawy.py
@@ -0,0 +1,43 @@
+print("Dominik")
+print(2+2)
+'''
+komentarz
+blokowy
+'''
+
+#potęgowanie
+
+potega = 2 ** 10
+print(potega)
+
+name = input("Podaj imie:\n")
+print("Witaj " + name + "!")
+
+surname = input("Podaj nazwisko\n")
+print("Twoje pełne imię i nazwisko to : {0} {1}".format(name, surname))
+
+print("Podaj swój wiek:")
+age = input()
+
+age1=20
+print(type(age1))
+print("Twój wiek to:", age1, "lata.")
+
+surname = "Kowalski"
+first_char = surname[0]
+print(first_char)
+print(len(surname))
+lastChar = surname[-1]
+print(lastChar)
+
+#konwersja
+x="5"
+print(type(x))
+x = int(x)
+print(type(x))
+
+y = 4
+print(type(y))
+y = y // 2
+print(y)
+print(type(y))
